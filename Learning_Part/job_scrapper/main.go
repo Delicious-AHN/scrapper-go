e2971c326a2639d975a1c87d88b0abb4910c2af3
Delicious-AHN <ahj3570@gmail.com>
2021-06-25 15:28:21 +0900

    job scrapper

--- Learning_Part/job_scrapper/main.go
+++ Learning_Part/job_scrapper/main.go
@@ -1,23 +1,99 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-var baseURL string = "https://kr.indeed.com/취업?q=python&limit=50"
+type extractedjob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
-	pages := getPages()
+	var jobs []extractedjob
+	c := make(chan []extractedjob)
+	totalpages := getPages()
+
+	for i := 0; i < totalpages; i++ {
+		go getPage(i, c)
+	}
+
+	for i := 0; i < totalpages; i++ {
+		extractedjobs := <-c
+		jobs = append(jobs, extractedjobs...)
+	}
+	writeJobs(jobs)
+	fmt.Println("Done, Extracted", len(jobs))
+}
+
+func getPage(page int, mainC chan<- []extractedjob) {
+	var jobs []extractedjob
+	c := make(chan extractedjob)
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	fmt.Println("Requesting : ", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body) // Body is bite => memory
+	checkErr(err)
+
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		go extractJob(card, c)
+	})
+
+	for i := 0; i < searchCards.Length(); i++ {
+		job := <-c
+		jobs = append(jobs, job)
+	}
+
+	mainC <- jobs
 }
 
 func getPages() int {
+	pages := 0
 	res, err := http.Get(baseURL)
 	checkErr(err)
 	checkCode(res)
 
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	return 0
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body) // Body is bite => memory
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
+	return pages
+}
+
+func extractJob(card *goquery.Selection, c chan<- extractedjob) {
+	id, _ := card.Attr("data-jk")
+	title := cleanString(card.Find(".title>a").Text())
+	location := cleanString(card.Find(".companyLocation").Text())
+	salary := cleanString(card.Find(".salary-snippet").Text())
+	summary := cleanString(card.Find(".summary").Text())
+	c <- extractedjob{
+		id:       id,
+		title:    title,
+		location: location,
+		salary:   salary,
+		summary:  summary}
 }
 
 func checkErr(err error) {
@@ -30,4 +106,28 @@ func checkCode(res *http.Response) {
 	if res.StatusCode != 200 {
 		log.Fatalln("Request failed with Status Code : ", res.StatusCode)
 	}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
+
+func writeJobs(jobs []extractedjob) {
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"ID", "TITLE", "LOACTION", "SALARY", "SUMMARY"}
+
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range jobs {
+		jobSlice := []string{"https://kr.indeed.com/viewjob?jk=" + job.id, job.title, job.location, job.salary, job.summary}
+		jwErr := w.Write(jobSlice)
+		checkErr(jwErr)
+	}
+
 }
